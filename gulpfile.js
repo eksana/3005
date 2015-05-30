5655e5c865449c1d6ee101188a153c4c7bc5f5db
dojdev <mr.dojdev@gmail.com>
2015-05-30 16:13:23 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require("gulp"),
+wiredep = require("Wiredep").stream,
 browserSync = require('browser-sync');
+
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-minify-css');
+
+
+gulp.task('wiredep', function () { 
+gulp.src('app/*.html')
+.pipe(wiredep())
+.pipe(gulp.dest('app'));
+});
 
 gulp.task('server', function () { 
 browserSync({
@@ -19,3 +32,16 @@ gulp.watch([
 });
 
 gulp.task('default', ['server', 'watch']);
+
+gulp.task('dist', function () {
+var assets = useref.assets();
+
+return gulp.src('app/*.html')
+.pipe(assets)
+.pipe(gulpif('*.js', uglify()))
+.pipe(gulpif('*.css', minifyCss()))
+.pipe(assets.restore())
+.pipe(useref())
+.pipe(gulp.dest('dist'));
+});
+
